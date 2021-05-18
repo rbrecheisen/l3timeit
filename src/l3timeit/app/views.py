4ebb17e220e49df1f6f81498bbdb920fc3d1527e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-05-18 15:51:27 +0200

    Uploading files works

--- src/l3timeit/app/views.py
+++ src/l3timeit/app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def upload(request):
+    files = request.FILES.getlist('files')
+    file_names = []
+    for f in files:
+        file_names.append(f.name)
+    return render(request, 'upload_success.html', context={'file_names': file_names})
+
+
+def download(request):
+    pass
