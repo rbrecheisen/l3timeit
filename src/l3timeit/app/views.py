42916cf837d04eeba3bb702dbd42b687a85dd3a5
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-05-18 16:19:45 +0200

    Files are stored but not using MEDIA_ROOT

--- src/l3timeit/app/views.py
+++ src/l3timeit/app/views.py
@@ -1,4 +1,6 @@
+import os
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
@@ -7,10 +9,13 @@ def index(request):
 
 def upload(request):
     files = request.FILES.getlist('files')
-    file_names = []
+    os.makedirs('/tmp/l3timeit', exist_ok=True)
+    storage = FileSystemStorage(location='/tmp/l3timeit')
+    file_urls = []
     for f in files:
-        file_names.append(f.name)
-    return render(request, 'upload_success.html', context={'file_names': file_names})
+        storage.save(f.name, f)
+        file_urls.append(f.name)
+    return render(request, 'upload_success.html', context={'file_names': file_urls})
 
 
 def download(request):
